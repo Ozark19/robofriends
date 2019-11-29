38e6b28daad6cf92ddd03710ab5319997085d519
Ozark19 <55114864+Ozark19@users.noreply.github.com>
2019-11-29 09:43:35 -0500

    Updates

---
+++ precache-manifest.e8e4ff201642c7c80f197312ecdb0a45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4ff27bcaa1543fa106c2ee483c73f1",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "9b0842f63dc8cdea63b2",
+    "url": "/robofriends/static/css/main.1c4efc56.chunk.css"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/robofriends/static/js/2.814c5b91.chunk.js"
+  },
+  {
+    "revision": "9b0842f63dc8cdea63b2",
+    "url": "/robofriends/static/js/main.fb70c512.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
